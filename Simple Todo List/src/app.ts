d0c0122715321bdf64c2b306e978b6606002d4c6
nesvalmo <nesvalmo@gmail.com>
2025-10-16 09:36:28 +0300

    Hard reset to the progress, the project was going kind of nowehere. Starting over with a better mindset.

--- Simple Todo List/src/app.ts
+++ Simple Todo List/src/app.ts
@@ -1,52 +1,22 @@
 // TYPE SECTION
 
-type stateType = "done" | "new" | "postponed";
+
+
+// INTERFACE SECTION
+
+
 
 // CLASS SECTION
 
-class Task {
-  taskDate: Date;
-  taskTitle: string;
-  taskDescription: string;
-  taskState: stateType;
-  constructor(inputTitle: string, inputDescription: string, inputState: stateType) {
-    this.taskTitle = inputTitle;
-    this.taskDescription = inputDescription;
-    this.taskState = inputState;
-    this.taskDate = new Date();
 
-    return this;
-  }
-}
 
 // GLOBAL VARIABLES SECTION
 
-let taskDatabase: Task[] = [];
+
 
 // FUNCTION SECTION
 
-function createTask(inputTitle: string, inputDescription: string): Task | undefined {
-    const newTask = new Task(inputTitle, inputDescription, "new");
-    taskDatabase.push(newTask);
-    return taskDatabase[taskDatabase.length - 1];
-}
 
-function openOverlay(): void {
-  const overlayElement = document.getElementById("addTaskOverlay");
-  if (overlayElement) {
-    overlayElement.style.display = "flex";
-  }
-}
 
-function closeOverlay(): void {
-  const overlayElement = document.getElementById("addTaskOverlay");
-  if (overlayElement) {
-    overlayElement.style.display = "none";
-  }
-}
+// IMMEDIATE APPLICATION
 
-// APPLICATION
-
-let temp = createTask("Attend class", "You have programming class at 8:00 tomorrow.");
-
-console.log(`${temp?.taskTitle} | ${temp?.taskState}\n${temp?.taskDate}\n${temp?.taskDescription}`);
